24893a0bbc1be387454aeff0dd574519e91b439f
liyulun_1 <1255315563@qq.com>
2018-11-19 23:29:38 +0800

    优化了函数，实现平均求时间

--- test_for_action.py
+++ test_for_action.py
@@ -55,110 +55,135 @@ print(len(L2))
 #classify L1 using different actions
 #L2[0:len[L2]]
 class_for_each=[]
+class_for_each_list=[]
 times_for_each=[]
 k=0
 dutation=[]
+dutation_all_action=[]
+dutation_for_each_action=[]
+average_for_all_action=[]
 for n in range(len(L2)):
-    #class_for_each.append(L2[n])
-    class_for_each=L2[n]
-    n=n+1
+    class_for_each=L2[n] 
+    
     print(class_for_each)
-    #print(class_for_each[0],class_for_each.[1])
     for item in L1:
         if re.findall(class_for_each,item):
-            class_for_each.append(item)
-            print('class_for_each',n,class_for_each)
-            l=len(class_for_each)
+            class_for_each_list.append(item)
+            l=len(class_for_each_list)
             print(l)
-            for i in range(len(class_for_each)):
-                times_for_each.append(class_for_each[i][0:12])
-                i=i+1
-                print('times_for_each=',times_for_each)
-                while k<=len(times_for_each):
-                    time_start=datetime.datetime.strptime(times_for_each[k], '%H:%M:%S,%f')
-                    time_end=datetime.datetime.strptime(times_for_each[k+1], '%H:%M:%S,%f')
-                    k=k+2
-                    dutation.append(time_end-time_start)
-                    print(time_end-time_start)
-                    if(k>=len(times_for_each)):
-                        break
-                print(dutation)
+    n=n+1
+
+    for i in range(len(class_for_each_list)):
+        times_for_each.append(class_for_each_list[i][0:12])
+        i=i+1
+    while k<=len(times_for_each):
+        time_start=datetime.datetime.strptime(times_for_each[k], '%H:%M:%S,%f')
+        time_end=datetime.datetime.strptime(times_for_each[k+1], '%H:%M:%S,%f')
+        k=k+2
+        dutation.append(time_end-time_start)
+        dutation_sec=(time_end-time_start).seconds+(time_end-time_start).microseconds/1000000 
+        dutation_for_each_action.append(dutation_sec)
+        if(k>=len(times_for_each)):
+            break
+    print(time_end-time_start)
+    print('dutation for',class_for_each,dutation,)
+    print('dutation_for_each_action for',class_for_each,dutation_for_each_action)
+    average_for_each_action=sum(dutation_for_each_action)/len(dutation_for_each_action)
+    
+    ####!!!
+    print('average_for_each_action',class_for_each,average_for_each_action)
+    average_for_all_action.append(average_for_each_action)
+    print('average_for_all_action',average_for_all_action)
+    #sort_average_for_all_action=sorted(average_for_all_action)
+    
+    class_for_each_list=[]
+    class_for_each=[]
+    times_for_each=[]
+    k=0
+    dutation=[]
+sort_average_for_all_action=sorted(average_for_all_action)
+print('sort_average_for_all_action',sort_average_for_all_action)
+##sort_average_for_all_action should contanst with its name "class_for_each"
+##verify need to find out more correctly
+##!!!
+
+
 ##########################################################################        
-Delay=[]
-Verify=[]
-SendLINMSG=[]
-VerifyFSATcp=[]
-Press=[]
-Comments=[]
-InputText=[]
-StopCommand=[]
-Click=[]
-UIAClick=[]
-ExecCommand=[]
-UIAVerify=[]
+# Delay=[]
+# Verify=[]
+# SendLINMSG=[]
+# VerifyFSATcp=[]
+# Press=[]
+# Comments=[]
+# InputText=[]
+# StopCommand=[]
+# Click=[]
+# UIAClick=[]
+# ExecCommand=[]
+# UIAVerify=[]
 
-print(L1)
-for item in L1:
-    if re.findall('Delay',item):
-        Delay.append(item)
-    if re.findall('SendLINMSG',item):
-        SendLINMSG.append(item)
-    if re.findall('VerifyFSATcp',item):
-        VerifyFSATcp.append(item)
-    if re.findall('Press',item):
-        Press.append(item)
-    if re.findall('Comments',item):
-        Comments.append(item)
-    if re.findall('InputText',item):
-        InputText.append(item)
-    if re.findall('StopCommand',item):
-        StopCommand.append(item)
-    if re.findall('Click',item):
-        Click.append(item)
-    if re.findall('UIAClick',item):
-        UIAClick.append(item)
-    if re.findall('ExecCommand',item):
-        ExecCommand.append(item)
-    if re.findall('UIAVerify',item):
-        UIAVerify.append(item) 
-    if re.findall('Verify',item):
-        Verify.append(item)
+# print(L1)
+# for item in L1:
+#     if re.findall('Delay',item):
+#         Delay.append(item)
+#     if re.findall('SendLINMSG',item):
+#         SendLINMSG.append(item)
+#     if re.findall('VerifyFSATcp',item):
+#         VerifyFSATcp.append(item)
+#     if re.findall('Press',item):
+#         Press.append(item)
+#     if re.findall('Comments',item):
+#         Comments.append(item)
+#     if re.findall('InputText',item):
+#         InputText.append(item)
+#     if re.findall('StopCommand',item):
+#         StopCommand.append(item)
+#     if re.findall('Click',item):
+#         Click.append(item)
+#     if re.findall('UIAClick',item):
+#         UIAClick.append(item)
+#     if re.findall('ExecCommand',item):
+#         ExecCommand.append(item)
+#     if re.findall('UIAVerify',item):
+#         UIAVerify.append(item) 
+#     if re.findall('Verify',item):
+#         Verify.append(item)
   
-print('Delay',Delay)
-print('SendLINMSG',SendLINMSG)
-print('VerifyFSATcp',VerifyFSATcp)
-print('Press',Press)
-print('Comments',Comments)
-print('InputText',InputText)
-print('StopCommand',StopCommand)
-print('Click',Click)
-print('UIAClick',UIAClick)
-print('ExecCommand',ExecCommand)
-print('UIAVerify',UIAVerify)
-print('Verify',Verify)    
+# print('Delay',Delay)
+# print('SendLINMSG',SendLINMSG)
+# print('VerifyFSATcp',VerifyFSATcp)
+# print('Press',Press)
+# print('Comments',Comments)
+# print('InputText',InputText)
+# print('StopCommand',StopCommand)
+# print('Click',Click)
+# print('UIAClick',UIAClick)
+# print('ExecCommand',ExecCommand)
+# print('UIAVerify',UIAVerify)
+# print('Verify',Verify)    
 
-#work out with each class
-times=[]
-l=len(Verify)
-print(l)
-for i in range(len(Verify)):
-    times.append(Verify[i][0:12])
-    i=i+1
-print('time=',times)
+# #work out with each class
+# times=[]
+# l=len(Verify)
+# print(l)
+# for i in range(len(Verify)):
+#     times.append(Verify[i][0:12])
+#     i=i+1
+# print('time=',times)
 
-sum=0
-k=0
-chazhilist=[]
-while k<=len(times):
-    time1=datetime.datetime.strptime(times[k], '%H:%M:%S,%f')
-    time2=datetime.datetime.strptime(times[k+1], '%H:%M:%S,%f')
-    k=k+2
-    chazhi=time2-time1
-    chazhilist.append(chazhi)
-    print(chazhi)
-    if(k>=len(times)):
-        break
-print(chazhilist)
+# sum=0
+# k=0
+# chazhilist=[]
+# while k<=len(times):
+#     time1=datetime.datetime.strptime(times[k], '%H:%M:%S,%f')
+#     time2=datetime.datetime.strptime(times[k+1], '%H:%M:%S,%f')
+#     k=k+2
+#     chazhi=time2-time1
+#     chazhilist.append(chazhi)
+#     print(chazhi)
+#     if(k>=len(times)):
+#         break
+# print(chazhilist)
 
 
 # for t in range(len(chazhilist)):
